a8100bcdbea9c8204e1cac0f81a108a45b132838
Sanskar <thakursanskaranhal@gmail.com>
2023-08-08 16:04:45 +0530

    added some features

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,11 +13,20 @@ export default function TextForm(props) {
         setText(event.target.value);
     }
 
+    // let defText = document.getElementById("myBox");
+    // let checkDefault = ()=>{
+    //     if (text === "Enter text here") {
+    //         defText.value = null;
+    //     }
+    // }
+
     const [text, setText] = useState("Enter text here");
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
+            {/* <textarea className="form-control" id="myBox" value={text} rows="8" onChange={handleOnChange} onClick={checkDefault}></textarea> */}
             <textarea className="form-control" id="myBox" value={text} rows="8" onChange={handleOnChange}></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
@@ -25,5 +34,14 @@ export default function TextForm(props) {
         <button className="btn btn-primary">Convert to uppercase</button>
         <button className="btn btn-primary">Convert to uppercase</button> */}
     </div>
+
+    <div className="container my-4">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words, {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} minutes to read this text</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
