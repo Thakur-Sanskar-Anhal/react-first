16ed9f7960e7b5e8221f4758745b74c8b68bcf82
Sanskar <thakursanskaranhal@gmail.com>
2023-08-14 20:30:08 +0530

    added about page

--- src/components/About.js
+++ src/components/About.js
@@ -10,36 +10,36 @@ export default function About(props) {
             <div className="accordion-item" style={{backgroundColor: props.mode==='dark'?'#49505A':'white' , color: props.mode==='dark'?'#D5D5D5':'black'}}>
                 <h2 className="accordion-header">
                 <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={{backgroundColor: props.mode==='dark'?'#353B43':'white' , color: props.mode==='dark'?'white':'black'}}>
-                    Accordion Item #1
+                Tinker.text
                 </button>
                 </h2>
                 <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
                 <div className="accordion-body">
-                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <strong>Tinker text is a text based utility app</strong> It allows the user to tinker with text. Manipulation of text in many text styles that are commonly used in coding like kebab case, Camel case, Lower case, Upper case, etc. It also allows the user to coppy the text to clip board to use it somewhere else and also provides text reciting. pause and play feature is good when you need to take a break from listening.
                 </div>
                 </div>
             </div>
             <div className="accordion-item" style={{backgroundColor: props.mode==='dark'?'#49505A':'white' , color: props.mode==='dark'?'#D5D5D5':'black'}}>
                 <h2 className="accordion-header">
                 <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={{backgroundColor: props.mode==='dark'?'#353B43':'white' , color: props.mode==='dark'?'white':'black'}}>
-                    Accordion Item #2
+                    Free to use
                 </button>
                 </h2>
                 <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                 <div className="accordion-body">
-                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <strong>Tinker.text is free for all</strong> It is a useful utility that anyone can access and use. It is developed by TSA- thakur sanskar anhal. It was created so a user could easily tinker with the text and make it easy for the users to edit the text and paste it directly to your code editor and code seamlesly.
                 </div>
                 </div>
             </div>
             <div className="accordion-item" style={{backgroundColor: props.mode==='dark'?'#49505A':'white' , color: props.mode==='dark'?'#D5D5D5':'black'}}>
                 <h2 className="accordion-header">
                 <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={{backgroundColor: props.mode==='dark'?'#353B43':'white' , color: props.mode==='dark'?'white':'black'}}>
-                    Accordion Item #3
+                    Recite
                 </button>
                 </h2>
                 <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
                 <div className="accordion-body">
-                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <strong>This recite feature is a verry helpful one.</strong> It is an distant feature that allows the user to coppy text from any source and paste it into the tinker.text edditor and reite it. You can simply coppy a word file and paste it to listen to it with the help of tinker.text and seamlesly do your other works while our browser compatable app recites your text so you can focus on productivity and multi tasking. It is a game changer feature that allows you to even pause the reciting when needed.
                 </div>
                 </div>
             </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,7 +15,7 @@ export default function NavBar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/"><b>Home</b></Link>
+                <Link className="nav-link" aria-current="page" to="/"><b>Home</b></Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/about"><b>{props.aboutText}</b></Link>
